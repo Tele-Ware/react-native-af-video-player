f8c78ee8dee75d6f944a439070c5d4a9f6ed04da
beshoy ekram <beshoyekram187@yahoo.com>
2020-05-18 15:20:53 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Video from './react-native-af-video-player/components/Video'
+import Video from './react-native-af-video/components/Video'
 export {
   Container,
   ScrollView
-} from './react-native-af-video-player/components'
+} from './react-native-af-video/components'
 
 export default Video
